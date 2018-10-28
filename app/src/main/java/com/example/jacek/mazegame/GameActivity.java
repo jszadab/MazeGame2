94b8b30d353df9435c3150f6a1791695946d0473
A019LPOL\jacek <jszadaborzyszkowski@gmail.com>
2018-10-28 14:57:56 +0100

    Added start screen with button

--- app/src/main/java/com/example/jacek/mazegame/GameActivity.java
+++ app/src/main/java/com/example/jacek/mazegame/GameActivity.java
@@ -2,18 +2,14 @@ package com.example.jacek.mazegame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-
-    public void startGame(View view) {
         setContentView(R.layout.activity_game);
     }
+
+
 }
